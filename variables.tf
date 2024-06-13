3420d71f6b7755b9d364ef2541c2c6588547a63a
Ahmedqayed <ahmed-kayed@hotmail.com>
2024-06-13 23:37:36 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "rg-devops-challenge"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  default     = "East US"
+}
